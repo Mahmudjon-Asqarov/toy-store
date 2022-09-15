52e80aec6ae2d97a6a677f37281ad1edd5ff2a99
Mahmudjon Asqarov <mahmidjonasqarov101@gmail.com>
2022-09-15 16:17:56 +0500

    added lang

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { setTranslations, setDefaultLanguage } from "react-multi-lang";
+import en from "./lang/en.json";
+import uz from "./lang/uz.json";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Shop from "./components/Shop";
 import { Toaster } from "react-hot-toast";
 import "./App.css";
+setTranslations({ uz, en });
+setDefaultLanguage(localStorage.getItem("lang") || "uz");
 function App() {
   return (
     <div className="app">
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,18 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import icon from "../assets/img/pngegg.png";
+import { useTranslation, setLanguage } from "react-multi-lang";
 
 function Header() {
+  const [lang, setLang] = useState(true);
+  const t = useTranslation();
   useEffect(() => {
     window.addEventListener("scroll", () => {
       let header = document.querySelector(".header");
       header.classList.toggle("sticky", window.scrollY > 0);
     });
   }, []);
+
   return (
     <div className="header">
       <NavLink to={"/"} className="logo">
@@ -16,7 +20,32 @@ function Header() {
       </NavLink>
       <ul>
         <li>
-          <NavLink to={"/"}>Products</NavLink>
+          <NavLink to={"/"}>{t("product")}</NavLink>
+        </li>
+        <li>
+          {lang ? (
+            <button
+              className="btn red accent-1"
+              onClick={() => {
+                setLanguage("uz");
+                localStorage.setItem("lang", "uz");
+                setLang(!lang);
+              }}
+            >
+              O‘zbekcha
+            </button>
+          ) : (
+            <button
+              className="btn red accent-1"
+              onClick={() => {
+                setLanguage("en");
+                localStorage.setItem("lang", "en");
+                setLang(!lang);
+              }}
+            >
+              English
+            </button>
+          )}
         </li>
       </ul>
     </div>
